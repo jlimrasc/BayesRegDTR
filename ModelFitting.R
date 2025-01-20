a5dea240d40b34599b60a9541da2c3f86675c4f7
jlim0208 <jlim0208@student.monash.edu>
2025-01-20 14:26:00 +1100

    Update ModelFitting.R
     - Put n into compute_Zt parameters
     - Rename sigmat into omegat

--- ModelFitting.R
+++ ModelFitting.R
@@ -1,19 +1,30 @@
 source("~/GitHub/BayesRegDTR/datagen.R")
 library(gtools)
-compute_Zt <- function(A, At_max, X, t) {
+compute_Zt <- function(A, At_max, X, t, n) {
     st <- function(A, key, t) {
-        return(apply(A[,,1:t-1] == rep(key, each = n), 1, all))
+        return(
+            if(is.vector(tempRes <- A[,1:(t-1)] == rep(key, each = n)))
+                tempRes
+            else
+                apply(tempRes, 1, all))
     }
     
-    Z_tilde <- X[,,1:t-1]
-    
+    Z_tilde <- matrix(X[,,1:(t-1)], nrow = n, ncol = (t-1) * p_t)
+
+    # Calculate permutations of a1, ..., an
     perms <- permutations(At_max, t-1, repeats.allowed = TRUE)
-    Zt <- matrix(0, nrow = n, ncol = (t-1) * nrow(perms))
+    
+    # Preallocate Zt
+    Zt <- matrix(0, nrow = n, ncol = (t-1) * p_t * nrow(perms))
+
+    # Compute Zt
     for (i in 1:nrow(perms)) {
-        colnum <- (i-1) * (t-1) + 1
-        Zt[, colnum:(colnum + t - 2)] <- st(A, perms[i,], t) * Z_tilde
+        colnum <- (i-1) * (t-1) * p_t + 1
+        Zt[, colnum:(colnum + (t-1) * p_t - 1)] <- st(A, perms[i,], t) * Z_tilde
     }
-    if ((ans <- ncol(Zt)) != (proper <- (t-1) * At_max^(t-1)))
+
+    # Check size
+    if ((ans <- ncol(Zt)) != (proper <- (t-1) * p_t * At_max^(t-1)))
         stop("Zt does not have the right amount of columns: ", ans, " != ", proper)
     
     return(Zt)
@@ -28,24 +39,24 @@ compute_RSSt <- function(Zt, thetat_hat, Xt) {
     sum((Xt - Zt %*% thetat_hat) ^ 2)
 }
 
-compute_sigmat <- function(Zt, tau) {
+compute_omegat <- function(Zt, tau) {
     t(Zt) %*% Zt + diag(tau, nrow = ncol(Zt))
 }
 
-compute_ct <- function(Zt, thetat_hat, sigmat) {
-    drop(t(thetat_hat) %*% t(Zt) %*% Zt %*% solve(sigmat) %*% t(Zt) %*% Zt %*% thetat_hat -
+compute_ct <- function(Zt, thetat_hat, omegat) {
+    drop(t(thetat_hat) %*% t(Zt) %*% Zt %*% solve(omegat) %*% t(Zt) %*% Zt %*% thetat_hat -
         t(thetat_hat) %*% t(Zt) %*% Zt %*% thetat_hat)
 }
 
-compute_mt <- function(Zt, thetat_hat, sigmat) {
-    solve(sigmat) %*% t(Zt) %*% Zt %*% thetat_hat
+compute_mt <- function(Zt, thetat_hat, omegat) {
+    solve(omegat) %*% t(Zt) %*% Zt %*% thetat_hat
 }
 
 t <- 3
 tau <- 10
-Zt          <- compute_Zt(A, At_max, X, t)
+Zt          <- compute_Zt(A, At_max, X, t, n)
 thetat_hat  <- compute_thetat_hat(Zt, X[,,t])
 RSSt        <- compute_RSSt(Zt, thetat_hat, X[,,t])
-sigmat      <- compute_sigmat(Zt, tau)
-ct          <- compute_ct(Zt, thetat_hat, sigmat)
-mt          <- compute_mt(Zt, thetat_hat, sigmat)
+omegat      <- compute_omegat(Zt, tau)
+ct          <- compute_ct(Zt, thetat_hat, omegat)
+mt          <- compute_mt(Zt, thetat_hat, omegat)
