9219d3dc8b410f823cbc430794f2b96a38393f27
Jeremy <160438890+jlimrasc@users.noreply.github.com>
2025-02-03 15:00:26 +1100

    Add check for T
     - Add checks to step 3 and 4 of datagen to check value of T

--- datagen.R
+++ datagen.R
@@ -16,38 +16,58 @@ A <- matrix(sample(1:At_len, n*T, replace = TRUE), nrow = n, ncol = T)
 X <- array(0, dim = c(n, p_t, T)) # Preallocate
 
 # Run first 3 lines fist bc can't be bothered making checks for numeric(0)s
-X[,,1] <- x_i1
+if (T > 0) {
+    X[, , 1] <- x_i1 
+}
+
+if (T > 1) {
 t <- 2
-X[,,t] <- rmvnorm(n, sigma = diag(0.5^2, p_t), mean = rep(0, p_t)) +
-    (A[,t-1] == 2) * (t * X[,,t-1]) + # If a==2
-    (A[,t-1] == 3) * (-t * X[,,t-1]) # Elif a==3
-
-t <- 3
-X[,,t] <- rmvnorm(n, sigma = diag(0.5^2, p_t), mean = rep(0, p_t)) +
-    (A[,t-1] == 2) * (t * X[,,t-1] - (t-1) * X[,,t-2]) + # If a==2
-    (A[,t-1] == 3) * (-t * X[,,t-1] + sqrt(t-1) * X[,,t-2]) # Elif a==3
-
-for (t in 4:T) {
-    # Could use dplyr instead
     X[,,t] <- rmvnorm(n, sigma = diag(0.5^2, p_t), mean = rep(0, p_t)) +
-        (A[,t-1] == 2) * (t * X[,,t-1] - (t-1) * X[,,t-2] + (t-2) * X[,,t-3]) + # If a==2
-        (A[,t-1] == 3) * (-t * X[,,t-1] + sqrt(t-1) * X[,,t-2] + sqrt(t-2) * X[,,t-3]) # Elif a==3
+        (A[,t-1] == 2) * (t * X[,,t-1]) + # If a==2
+        (A[,t-1] == 3) * (-t * X[,,t-1]) # Elif a==3
+}
+
+if (T > 2) {
+t <- 3
+    X[,,t] <- rmvnorm(n, sigma = diag(0.5^2, p_t), mean = rep(0, p_t)) +
+        (A[,t-1] == 2) * (t * X[,,t-1] - (t-1) * X[,,t-2]) + # If a==2
+        (A[,t-1] == 3) * (-t * X[,,t-1] + sqrt(t-1) * X[,,t-2]) # Elif a==3
+}
+
+if (T > 3) {
+    for (t in 4:T) {
+        # Could use dplyr instead
+        X[,,t] <- rmvnorm(n, sigma = diag(0.5^2, p_t), mean = rep(0, p_t)) +
+            (A[,t-1] == 2) * (t * X[,,t-1] - (t-1) * X[,,t-2] + (t-2) * X[,,t-3]) + # If a==2
+            (A[,t-1] == 3) * (-t * X[,,t-1] + sqrt(t-1) * X[,,t-2] + sqrt(t-2) * X[,,t-3]) # Elif a==3
+    }
 }
 
 # Normalise X
-X <- apply(X, 2:3, function(z) (z-mean(z))/sd(z))
+if (T > 0) {
+    X <- apply(X, 2:3, function(z) (z-mean(z))/sd(z))
+}
 
 # Step 4
 mi <- 3
-t <- 1
-mi <- mi + (A[,t] == 2) * (sin(10*t) * X[,,t]) + (A[,t] == 3) * (cos(10*t) * X[,,t])
-t <- 2
-mi <- mi + (A[,t] == 2) * (sin(10*t) * X[,,t] - sin(10*t - 10) * X[,,t-1]) + 
-    (A[,t] == 3) * (cos(10*t) * X[,,t] - cos(10*t - 10) * X[,,t-1])
-for (t in 3:T) {
-    mi <- mi + 
-        (A[,t] == 2) * (sin(10*t) * X[,,t] - sin(10*t - 10) * X[,,t-1] + sin(10*t - 20) * X[,,t-2]) +
-        (A[,t] == 3) * (cos(10*t) * X[,,t] - cos(10*t - 10) * X[,,t-1] + sqrt(abs(cos(10*t - 20))) * X[,,t-2])
+
+if (T > 0) {
+    t <- 1
+    mi <- mi + (A[,t] == 2) * (sin(10*t) * X[,,t]) + (A[,t] == 3) * (cos(10*t) * X[,,t])
+}
+
+if (T > 1) {
+    t <- 2
+    mi <- mi + (A[,t] == 2) * (sin(10*t) * X[,,t] - sin(10*t - 10) * X[,,t-1]) + 
+        (A[,t] == 3) * (cos(10*t) * X[,,t] - cos(10*t - 10) * X[,,t-1])
+}
+
+if (T > 2) {
+    for (t in 3:T) {
+        mi <- mi + 
+            (A[,t] == 2) * (sin(10*t) * X[,,t] - sin(10*t - 10) * X[,,t-1] + sin(10*t - 20) * X[,,t-2]) +
+            (A[,t] == 3) * (cos(10*t) * X[,,t] - cos(10*t - 10) * X[,,t-1] + sqrt(abs(cos(10*t - 20))) * X[,,t-2])
+    }
 }
 
 yi <- rnorm(n, mean = mi, sd = 1)
