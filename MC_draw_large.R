f25f87bbe73bed61b2ff2ebfcac6193714093405
Jeremy <160438890+jlimrasc@users.noreply.github.com>
2025-02-11 15:03:39 +1100

    Fix draw_Wt_b
     - Fix incorrect syntax in function
     - Remove unused parameter
     - Add correct type (vector) to V0 input and usage of it

--- MC_draw_large.R
+++ MC_draw_large.R
@@ -2,14 +2,14 @@ library(mvtnorm)
 library(MixMatrix)
 library(tictoc)
 compute_MC_draws <- function(D, tau, At_mags, B, nu0, V0) {
-    draw_sigmat_b <- function(Zt, Xt, Mnt, nu0, V0, tau, n) {
-        Vn <- V0 + t(Xt - Zt %*% Mnt) %*% (Xt - Zt %*%Mnt) + tau * t(Mnt) %*% Mnt
-        return(1 / rWishart(B, df = n + nu0, Sigma = Vn))
+    draw_sigmat_b <- function(Zt, Xt, Mnt, nu0, V0, tau, n, B, t) {
+        Vn <- V0[[t]] + t(Xt - Zt %*% Mnt) %*% (Xt - Zt %*%Mnt) + tau * t(Mnt) %*% Mnt
+        test <- rWishart(B, df = n + nu0, Sigma = solve(Vn))
+        return(apply(test, 3, solve, simplify = FALSE))
     }
     
-    draw_Wt_b <- function(Zt, omegat, Mnt, tau, sigmat_b) {
-        # browser()
-        lapply(FUN = function(z) rmatrixnorm(1, mean = Mnt, U = solve(omegat), z), sigmat_b)
+    draw_Wt_b <- function(omegat, Mnt, tau, sigmat_b) {
+        lapply(sigmat_b, FUN = function(z) rmatrixnorm(1, mean = Mnt, U = solve(omegat), z))
     }
     
     X <- Data[-1]
@@ -24,12 +24,12 @@ compute_MC_draws <- function(D, tau, At_mags, B, nu0, V0) {
         Mnt <- solve(omegat) %*% t(Zt) %*% X[[t]]
         
         # Draw
-        sigmat_b <- draw_sigmat_b(Zt, X[[t]], Mnt, nu0, V0, tau, n)
-        # browser()
+        sigmat_b <- draw_sigmat_b(Zt, X[[t]], Mnt, nu0, V0, tau, n, B, t)
+        
         tic(t)
-        Wt_b <- draw_Wt_b(Zt, omegat, Mnt, tau, sigmat_b)
+        Wt_b <- draw_Wt_b(omegat, Mnt, tau, sigmat_b)
         toc()
-        # browser()
+        
     
         # Store
         sigmat_b_list[[t]]  <- sigmat_b
@@ -39,6 +39,7 @@ compute_MC_draws <- function(D, tau, At_mags, B, nu0, V0) {
         return(list(sigmat_b_list, Wt_b_list))
 }
 
-source("~/GitHub/BayesRegDTR/datagen_multivariate.R")
-source("~/GitHub/BayesRegDTR/ModelFitting.R")
-res <- compute_MC_draws(D = Data, tau = 0.01, At_mags = 1, B = 10000, nu0 = 3, V0 = 4)
+# source("~/GitHub/BayesRegDTR/datagen_multivariate.R")
+# source("~/GitHub/BayesRegDTR/ModelFitting.R")
+res <- compute_MC_draws(D = Data, tau = 0.01, At_mags = rep(3, T), B = 10000, nu0 = 3, 
+                        V0 = mapply(diag, p_list, SIMPLIFY = FALSE))
